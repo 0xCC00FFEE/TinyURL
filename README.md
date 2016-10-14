9fa0e4dd68acc663a15eb4af3da56a8da65327bc
Abdullah Mohamed <secmania@hotmail.com>
2016-10-14 18:06:34 +0200

    Created Files README.md and tinyurl.py

---
+++ README.md
@@ -0,0 +1,8 @@
+<b><h1>TinyURL</h1><br><br></b>
+
+A Python script that uses tinyurl.com API to shorten URL's directly from command line.<br><br>
+
+<b>Usage:</b><br>
+./tinyurl <URL>
+
+
---
+++ tinyurl.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+try:
+    import urllib2,urllib,os,sys
+except ImportError,err:
+    print "[~]Invalid Module(s) !"
+    print "[~]%s ." % err
+    sys.exit(0)
+
+def usage():
+    print """
+#################################################
+### 		TinyURL v0.1		      ###
+#################################################
+
+Usage :
+*******
+
+./tiny <uri-goes-here>
+
+"""
+
+url="http://tinyurl.com/api-create.php?url="
+#input=raw_input("Enter the link you want to tiny : ")
+if len(sys.argv) != 2:
+    usage();
+    sys.exit(0);
+if str(sys.argv[1]).find("http://") == -1 and  str(sys.argv[1]).find("https://") == -1:
+	sys.argv[1]="http://"+sys.argv[1]
+req=urllib2.urlopen(url+str(sys.argv[1]))
+data=req.read()
+print "\n[~] URL : "+str(data) + "\n"
+
+
